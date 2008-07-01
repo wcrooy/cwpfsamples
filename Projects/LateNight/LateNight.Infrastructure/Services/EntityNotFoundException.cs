03b797b5b76011d344703f7a8baf5bb08a2370bb
brett.ryan <brett.ryan@localhost>
2008-07-01 06:10:52 +0000

    Added new service level exceptions recommended for services to use.

---
+++ Projects/LateNight/LateNight.Infrastructure/Services/EntityNotFoundException.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * EntityNotFoundException.cs    6/30/2008 3:49:37 PM
+ *
+ * Copyright 2008  All rights reserved.
+ * Use is subject to license terms
+ *
+ * Author: bryan
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+
+namespace BrettRyan.LateNight.Services {
+
+    /// <summary>
+    ///
+    /// </summary>
+    [Serializable]
+    public class EntityNotFoundException : Exception, ISerializable {
+
+        /// <summary>
+        /// Creates a new instance of <c>EntityNotFoundException</c>.
+        /// </summary>
+        public EntityNotFoundException(Type entityType)
+            : base() {
+            EntityType = entityType.Name;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <c>EntityNotFoundException</c>.
+        /// </summary>
+        public EntityNotFoundException(Type entityType, string message)
+            : base(message) {
+            EntityType = entityType.Name;
+        }
+
+        public EntityNotFoundException(
+            SerializationInfo info, StreamingContext context)
+            : base(info, context) {
+            EntityType = info.GetString("EntityType");
+        }
+
+        public override void GetObjectData(
+            SerializationInfo info, StreamingContext context) {
+            info.AddValue("EntityType", EntityType);
+            base.GetObjectData(info, context);
+        }
+
+        /// <summary>
+        /// Full type name of entity.
+        /// </summary>
+        public string EntityType {
+            get;
+            private set;
+        }
+
+    }
+
+}
+
