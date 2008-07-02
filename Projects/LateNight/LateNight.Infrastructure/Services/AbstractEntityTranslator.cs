d7406f75ea0e8d5553bb2284c99a3e6412bdd007
brett.ryan <brett.ryan@localhost>
2008-07-02 06:52:18 +0000

    Added abstract implementations for entity translators. This whole pattern has been completely based on the SCSF framework pattern by the same name. I will be refactoring this later.

---
+++ Projects/LateNight/LateNight.Infrastructure/Services/AbstractEntityTranslator.cs
@@ -0,0 +1,51 @@
+﻿//===============================================================================
+// Microsoft patterns & practices
+// Smart Client Software Factory
+//===============================================================================
+// Copyright (C) Microsoft Corporation.  All rights reserved.
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE.
+//===============================================================================
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+//===============================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BrettRyan.LateNight.Services {
+
+    /// <summary>
+    ///
+    /// </summary>
+    public abstract class AbstractEntityTranslator : IEntityTranslator {
+
+        #region IEntityTranslator Members
+
+        public abstract bool CanTranslate(Type targetType, Type sourceType);
+
+        public bool CanTranslate<TTarget, TSource>() {
+            return CanTranslate(typeof(TTarget), typeof(TSource));
+        }
+
+        public abstract object Translate(
+            IEntityTranslatorService service, Type targetType, object source);
+
+        public TTarget Translate<TTarget>(IEntityTranslatorService service, object source) {
+            return (TTarget)Translate(service, typeof(TTarget), source);
+        }
+
+        #endregion
+
+    }
+
+}
+
