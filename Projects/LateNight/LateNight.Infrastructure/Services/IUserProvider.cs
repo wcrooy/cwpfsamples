2b60a4646aa2520c79e5e59c313f9faed229722b
brett.ryan <brett.ryan@localhost>
2008-07-02 04:32:25 +0000

    Added new interfaces to support a modular security/principal service that will be created before the bootstrapper is called.

---
+++ Projects/LateNight/LateNight.Infrastructure/Services/IUserProvider.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * IUserProvider.cs    7/2/2008 2:23:32 PM
+ *
+ * Copyright 2008 Brett Ryan. All rights reserved.
+ * Use is subject to license terms
+ *
+ * Author: bryan
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BrettRyan.LateNight.Services {
+
+    /// <summary>
+    ///
+    /// </summary>
+    public interface IUserProvider {
+
+        string ApplicationRole { get; }
+
+        bool IsApplicationRoleRequired { get; }
+
+        bool Authenticate(string user, string pass);
+
+        string[] GetRoles(string user);
+
+    }
+
+}
+
