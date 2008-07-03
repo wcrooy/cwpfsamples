b1eb64666c4eff1e835e5a0793d633b96d08a6b8
brett.ryan <brett.ryan@localhost>
2008-07-03 06:48:58 +0000

    Update to file header comment and some code documentation updates.

--- Projects/LateNight/LateNight/Services/EntityTranslatorService.cs
+++ Projects/LateNight/LateNight/Services/EntityTranslatorService.cs
@@ -1,19 +1,29 @@
-﻿//===============================================================================
-// Microsoft patterns & practices
-// Smart Client Software Factory
-//===============================================================================
-// Copyright (C) Microsoft Corporation.  All rights reserved.
-// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
-// OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
-// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-// FITNESS FOR A PARTICULAR PURPOSE.
-//===============================================================================
-// The example companies, organizations, products, domain names,
-// e-mail addresses, logos, people, places, and events depicted
-// herein are fictitious.  No association with any real company,
-// organization, product, domain name, email address, logo, person,
-// places, or events is intended or should be inferred.
-//===============================================================================
+﻿/*
+ * EntityTranslatorService.cs    7/2/2008 15:48:12
+ * 
+ * Copyright 2008 Brett Ryan. All rights reserved.
+ * Use is subject to license terms
+ *
+ * Implementation is based from guidance provided in Microsoft patterns &
+ * practices - Smart Client Software Factory.
+ * 
+ * Original copyright notice.
+ * ==========================================================================
+ * Copyright (C) Microsoft Corporation.  All rights reserved.
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+ * OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ * ==========================================================================
+ * The example companies, organizations, products, domain names,
+ * e-mail addresses, logos, people, places, and events depicted
+ * herein are fictitious.  No association with any real company,
+ * organization, product, domain name, email address, logo, person,
+ * places, or events is intended or should be inferred.
+ * ==========================================================================
+ * 
+ * Author: Brett Ryan
+ */
 
 using System;
 using System.Collections.Generic;
